ae68d1dc3557a9ac0b4a3bc1fb0ced5f00916bab
pravinjenkins <pravinnikure300@gmail.com>
2024-03-11 15:01:14 +0530

    Adding service and controller for rest template

--- src/main/java/com/app/pravin/vendorservice/SpringCloudVendorServiceApplication.java
+++ src/main/java/com/app/pravin/vendorservice/SpringCloudVendorServiceApplication.java
@@ -1,9 +1,11 @@
-package com.app.pravin.chartservice;
+package com.app.pravin.vendorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudVendorServiceApplication {
 
 	public static void main(String[] args) {
