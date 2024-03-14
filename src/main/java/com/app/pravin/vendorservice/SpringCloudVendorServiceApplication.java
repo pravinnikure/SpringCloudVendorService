259400ebffc0650e77c176c548a0d627b0406050
pravinjenkins <pravinnikure300@gmail.com>
2024-03-14 13:15:25 +0530

    Adding actuator for refresh scope

--- src/main/java/com/app/pravin/vendorservice/SpringCloudVendorServiceApplication.java
+++ src/main/java/com/app/pravin/vendorservice/SpringCloudVendorServiceApplication.java
@@ -3,12 +3,13 @@ package com.app.pravin.vendorservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient //used for register classes
 @EnableFeignClients   // TO generage the client code
-
+@RefreshScope
 public class SpringCloudVendorServiceApplication {
 
 	public static void main(String[] args) {
